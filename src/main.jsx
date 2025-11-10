8634aba09a7b87cdd9471fd2e44bbd209a4d283d
RickD <cryptowealthinvestments@gmail.com>
2025-11-09 17:38:16 -0800

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+function App(){ return <div style={{color:'#fff',background:'#0b0f1a',minHeight:'100vh'}}>Boot check OK</div>; }
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
